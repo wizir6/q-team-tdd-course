80980a1bd4376362793346068f76731ebd4c36eb
Semyon Boyko <keystone@apriorit.com>
2019-03-10 22:11:04 +0200

    Implemented test to check the SplitString function with invalid params.

--- tdd_intro/homework/04_weather_client/test.cpp
+++ tdd_intro/homework/04_weather_client/test.cpp
@@ -159,6 +159,14 @@ private:
 
 };
 
+TEST(WeatherClient, get_average_temperature_with_invalid_value)
+{
+    ServerMock server;
+    ConcreteWeatherClient client;
+
+    EXPECT_THROW(client.GetAverageTemperature(server, "01.09.2017"), std::invalid_argument);
+}
+
 TEST(WeatherClient, get_proper_average_temperature_in_01_09_2018)
 {
     ServerMock server;
